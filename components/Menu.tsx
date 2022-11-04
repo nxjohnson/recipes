24cc452149bde2f36d3d9d8b7cdc4342578008d9
Neil <neiljohnson92@gmail.com>
2022-11-03 20:38:32 -0700

    Created foundation for menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,34 +1,46 @@
-import { FunctionComponent } from "react";
+import Link from "next/link";
+import React, { FunctionComponent, ReactPortal } from "react";
 import ReactDom from "react-dom";
 import styles from "../styles/Menu.module.css"
+import PageHeader from './layouts/PageHeader'
 
 interface MenuProps {
   showMenu: boolean,
+  toggleMenu: VoidFunction
 }
 
-const Menu = ({ showMenu }: MenuProps) => {
+const Menu = ({ showMenu, toggleMenu }: MenuProps): ReactPortal | null => {
   const categories: string[] = ['Meat', 'Pasta', 'Soups', 'Veggies', 'Bread', 'Desserts', 'View All'];
   if (!showMenu) {
-    return;
+    return null;
   }
 
   return ReactDom.createPortal(
     <div className={styles.menuPortal}>
-      <h1 className={styles.title}>Contents</h1>
-      <div className={styles.categoryList}>
-        <div className={styles.category}>
-          <span>Search</span>
-          <span>00</span>
-        </div>
-        {categories.map((category, index) => {
-          return (
-            <div key={category} className={styles.category}>
-              <span>{category}</span>
-              <span>{'0' + (index + 1)}</span>
+      <PageHeader header="Contents">
+        <div className={styles.categoryList}>
+          <div className={styles.category}>
+            <span>Search</span>
+            <span>00</span>
+          </div>
+          <Link href={'/'} onClick={() => toggleMenu()}>
+            <div className={styles.category}>
+              <span>Home</span>
+              <span>00</span>
             </div>
-          )
-        })}
-      </div>
+          </Link>
+          {categories.map((category, index) => {
+            return (
+              <Link key={category} href={`/recipes`} onClick={() => toggleMenu()}>
+                <div className={styles.category}>
+                  <span>{category}</span>
+                  <span>{'0' + (index + 1)}</span>
+                </div>
+              </Link>
+            )
+          })}
+        </div>
+      </PageHeader>
     </div>, document.getElementById("menu-portal") as HTMLElement
   )
 }
