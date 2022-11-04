27823a62877718546ca5ca7fb5f3f23a52a1a641
Neil <neiljohnson92@gmail.com>
2022-11-03 20:47:29 -0700

    Added RootLayout to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import RootLayout from '../components/layouts/RootLayout'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -12,7 +13,10 @@ type AppPropsWithLayout = AppProps & {
 }
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
 
-  return (getLayout(<Component {...pageProps} />))
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  )
 }
