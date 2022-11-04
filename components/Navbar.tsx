df7182112345fedceaaa787c76eff23f6087d461
Neil <neiljohnson92@gmail.com>
2022-11-03 20:43:01 -0700

    New icons and added Next Link for add-recipe

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { useState, FunctionComponent } from "react";
+import Link from "next/link";
 import styles from '../styles/Navbar.module.css'
+import { MdOutlineMenu, MdOutlineShoppingBag, MdOutlinePostAdd } from "react-icons/md";
 import Menu from "./Menu";
 
 const Navbar: FunctionComponent = () => {
-  const [showMenu, setShowMenu] = useState<Boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
-  const openMenu = () => {
+  const toggleMenu = () => {
     showMenu ? setShowMenu(false) : setShowMenu(true);
   }
 
@@ -13,11 +15,14 @@ const Navbar: FunctionComponent = () => {
     <>
       <nav className={styles.navbar}>
         <div className={styles.navIcons}>
-          <span className={`material-symbols-outlined ${styles.hamburgerMenu}`} onClick={openMenu}>menu</span>
-          <span className={`material-symbols-outlined ${styles.shoppingBag}`}>shopping_bag</span>
+          <MdOutlineMenu className={styles.hamburgerMenu} onClick={toggleMenu} />
+          <Link href={'/add-recipe'}>
+            <MdOutlinePostAdd className={styles.shoppingBag} />
+          </Link>
+          <MdOutlineShoppingBag className={styles.shoppingBag} />
         </div>
       </nav>
-      <Menu showMenu={showMenu} />
+      <Menu showMenu={showMenu} toggleMenu={toggleMenu} />
     </>
   )
 }
